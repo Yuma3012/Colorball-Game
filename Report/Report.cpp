e34db3c9051fcee2731d706f301fcf130757c6c9
Yuma3012 <yumakobayashi1206@icloud.com>
2023-11-17 17:10:24 +0900

    colorとradiusをランダム化

--- Report/Report.cpp
+++ Report/Report.cpp
@@ -1,21 +1,53 @@
 #include "def.h"
 #include "mlib.h"
-
+#include <time.h>
+#include <cstdlib>
 #define g 9.8
-
+// 色と値を表す構造体
+struct ColorValue {
+	int color;
+	int radius;
+};
 int main(int Number) {
-	int m, n, mm = 0, nn = 0, r = 10;
+	int m, n, mm = 0, nn = 0;
 	double x = 0, y = 95, w = 100, h = 100;
 	double vy = 0, dt = 5e-3, vx;
 	double e = 1;
 	int resetScreen = 0; // 画面をリセットするフラグ
 	int found = 0;
 
-	while (1) { //While()文は()内が0になるまで繰り返すので、例では無限ループ,物体がラインに到達するまで
+	// 色と値のペアを配列に格納
+	struct ColorValue colorValues[] = {
+		{0, 70}, //黒
+		{1, 60}, //赤
+		{2, 50}, //緑
+		{3, 40}, //青
+		{4, 30}, //黄色
+		{5, 20}, //水色
+		{6, 10}  //ピンク
+	};
+	// 乱数生成のためのシードを設定
+	srand(time(NULL));
+
+	// ランダムにインデックスを選択
+	int randomIndex = rand() % (sizeof(colorValues) / sizeof(colorValues[0]));
+
+	// 選択された色と半径のペアを取得
+	int color = colorValues[randomIndex].color;
+	int r = colorValues[randomIndex].radius;
+
+		//球が下に当たったら処理を終了する
+	// ラベルを使用してswitch内でループを制御する
+main_loop:
+
+	while (1) { //While()文は()内が0になるまで繰り返すので、1では無限ループ,物体がラインに到達するまで
 		short pl, pr, pd;
 		pl = GetAsyncKeyState(VK_LEFT);//左キーの状態入力
 		pr = GetAsyncKeyState(VK_RIGHT);//右キーの状態入力
 		pd = GetAsyncKeyState(VK_DOWN);//下キーの状態入力
+
+		//case1の処理がおわったらcase0の処理をおこなう
+		
 		switch (Number) {
 		case 0:
 
@@ -23,9 +55,12 @@ int main(int Number) {
 				Plot_pen(0, 2, 7);  //白色に設定（バックと同じ）
 				Rect(-w, h, w, -h, 1);
 			}
+
 			Plot_pen(0, 2, 7);  //白色に設定（バックと同じ）
 			Circle(mm - r, nn - r, mm + r, nn + r, 1);        //過去の位置に描かれた円を消去
-
+			
+			Plot_pen(0, 2, 1);
+			Line(-sGW.w, h-60, sGW.w, h-60); //デッドライン
 			if (pl < 0) {
 				x = x - 0.1;     //左キーが押されたときの処理
 				if (x < -w) {
@@ -41,10 +76,9 @@ int main(int Number) {
 
 			m = sGW.w / 2 * (x / w + 1);//実座標をピクセル座標に変換sgw960/2*(0/101)=480
 			n = sGW.h / 2 * (1 - y / h);//実座標をピクセル座標に変換620/2*(1-95/100)=291.4
-			Plot_pen(0, 2, 3);  //緑色に指定   
+			Plot_pen(0, 2, color);  //緑色に指定   
 			Circle(m - r, n - r, m + r, n + r, 1);        //新しい位置に円を描画
-			Plot_pen(0, 2, 1);
-			Line(-sGW.w, n+10, sGW.w, n+10); //デッドライン
+
 
 
 
@@ -56,7 +90,7 @@ int main(int Number) {
 			}
                 
 			if (found == 1) {
-				while (y >= -h+0.5*r) {
+				while (y >= -h) {
 					Plot_pen(0, 2, 7); //白色に設定（バックと同じ）
 					Circle(mm - r, nn - r, mm + r, nn + r, 1); //過去の玉を消去
 					y = y + vy * dt - g * dt * dt / 2;  //  z座標更新
@@ -78,7 +112,7 @@ int main(int Number) {
 						x = -w;
 					} //zは左にめり込んでいるので-wに修正
 
-					//aaa
+					
 
 					m = sGW.w * (1 + x / w) / 2;  //実座標をピクセル座標に変換
 					n = sGW.h * (1 - y / h) / 2;     //実座標をピクセル座標に変換
@@ -93,15 +127,21 @@ int main(int Number) {
 					nn = n; //新しいピクセル座標を過去のピクセル座標に
 				}
 				found = 0;
+				
 			}
 			Refresh();        //画面更新
-			
-		break;
+			//Printf("処理完了");
+		    break;
 		case 1:
-			Plot_pen(0, 2, 7);  //白色に設定（バックと同じ）
-			Rect(-w, h, w, -h, 1);
+			// 画面をリセットする
+			resetScreen = 1;
+			Plot_pen(0, 2, 1);  //白色に設定（バックと同じ）
+			//Rect(-w, h, w, -h, 1);
+			Line(-sGW.w, h - 60, sGW.w, h - 60); //デッドライン
 			break;
 		}
+		// switch の先頭に戻る
+		goto main_loop;
 	}
 }
 
